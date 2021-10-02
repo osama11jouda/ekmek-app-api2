3073cd302d307fbdf3096ff89bc7761663cf2925
osama jouda <oj.turk@gmail.com>
2021-10-02 20:53:09 +0300

    Update Files For Heroku

--- default_config.py
+++ default_config.py
@@ -2,12 +2,12 @@ import os
 from datetime import timedelta
 
 DEBUG = True
-SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+SQLALCHEMY_DATABASE_URI = 'sqlite:///data.db'  # os.environ.get('DATABASE_URL')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-SECRET_KEY = os.environ.get('APP_SECRET_KEY')
-JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
+SECRET_KEY = 'Ekmek_App_Api2_2021-10-01'  # os.environ.get('APP_SECRET_KEY')
+JWT_SECRET_KEY = 'Ekmek_App_Api2_2021-10-01'  # os.environ.get('JWT_SECRET_KEY')
 JWT_BLACKLIST_ENABLED = True
 JWT_BLACKLIST_TOKEN_CHECKS = ['access', 'refresh']
 UPLOADED_IMAGES_DEST = os.path.join('static', 'images')
-JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=1)
+JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=12)
 
