924c89675d5ac5bed933b44682d251ccc9db1fe6
osama jouda <oj.turk@gmail.com>
2021-10-03 07:14:13 +0300

    Updating Image Uploading

--- default_config.py
+++ default_config.py
@@ -2,7 +2,7 @@ import os
 from datetime import timedelta
 
 DEBUG = True
-SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL2')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = os.environ.get('APP_SECRET_KEY')
 JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
