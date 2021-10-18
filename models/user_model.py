270977cad235c79082895c221fc6dcc7c9aa2960
osama jouda <oj.turk@gmail.com>
2021-10-18 08:28:53 +0300

    update user model

--- models/user_model.py
+++ models/user_model.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timezone
+import time
 from typing import List
 
 from db import db
@@ -15,7 +15,7 @@ class UserModel(db.Model):
     password = db.Column(db.String(255), nullable=False)
     email = db.Column(db.String(255), nullable=True)
     user_image = db.Column(db.String(255), nullable=True)
-    registered_at = db.Column(db.DateTime, nullable=False, default=datetime.now(timezone.utc).astimezone())
+    registered_at = db.Column(db.DateTime, nullable=False, default=time.strftime('%A %B, %d %Y %H:%M:%S'))
     is_admin = db.Column(db.Boolean, nullable=False, default=False)
     current_balance = db.Column(db.Float(precision=2), nullable=True, default=0.0)
 
